821a240b87ee3bdd52f0ed9ed70d042f25a1b753
Martin Dressel <martindressel@hotmail.com>
2013-10-21 10:18:26 +0200

    Coawment

--- game/game.java
+++ game/game.java
@@ -9,7 +9,7 @@ public class game
 	public static ArrayList<Player> players;
 	public static Player me;
 
-	public static void main(String[] args) throws Exception
+	 public static void main(String[] args) throws Exception
 	{
 		System.out.println("Indtast dit spillernavn");
 		BufferedReader bName = new BufferedReader(new InputStreamReader(System.in));

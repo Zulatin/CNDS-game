6d63908afe033de6e33a8a88f138447a124fd9fd
Mike Raunsbæks <raunsbaek@raunsbaek.local>
2013-10-21 09:45:23 +0200

    Comments added

--- game/game.java
+++ game/game.java
@@ -4,28 +4,31 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class game {
-
-	/**
-	 * @param args
-	 * 
-	 */
+public class game
+{
 	public static ArrayList<Player> players;
 	public static Player me;
 
 	public static void main(String[] args) throws Exception
 	{
 		System.out.println("Indtast dit spillernavn");
-		BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
-		String in = b.readLine();
+		BufferedReader bName = new BufferedReader(new InputStreamReader(System.in));
 
+		// Make player list
 		players = new ArrayList<Player>();
-		me = new Player(in);
+
+		// Create you
+		me = new Player(bName.readLine());
+
+		// Add players to play list
 		players.add(me);
 		players.add(new Player("FUP"));
 
-		ScoreList s = new ScoreList(players);
-		s.setVisible(true);
-		gameplayer g = new gameplayer(me, s, players);
+		// Create a scorelist
+		ScoreList scoreList = new ScoreList(players);
+		scoreList.setVisible(true);
+
+		// Spawn new player - thats you
+		new gameplayer(me, scoreList, players);
 	}
 }

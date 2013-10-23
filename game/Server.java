bdd1b54b34e696fc52d4dbc5288cbdff7c77f8b7
Martin Dressel <martindressel@hotmail.com>
2013-10-23 11:13:53 +0200

    Changed IngoingServer/OutGoingServer/Player/Server/gameplayer

--- game/Server.java
+++ game/Server.java
@@ -1,23 +1,61 @@
 package game;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
+	private static ArrayList<Player> players;
+	private static gameplayer board;
 	
 	public static void main(String[] args)throws Exception {
+		players = new ArrayList<Player>();
+		ScoreList scoreList = new ScoreList(players);
+		board = new gameplayer(scoreList, players);
 		String clientSentence;
 		String sentence;
+		DataOutputStream outToClient;
 		ServerSocket welcomeSocket = new ServerSocket(7531);
-//		gameplayer board = new gameplayer();
-		IngoingServer in = new IngoingServer();
-		OutgoingServer out = new OutgoingServer();
-		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(System.in));
+
+		IngoingServer in;
+		OutgoingServer out;
+		BufferedReader inFromClient;
 		
 		while(true){
+			Socket connectionSocket = welcomeSocket.accept();
+			outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			clientSentence = inFromClient.readLine();
+			
+			checkPlayer(clientSentence, inFromClient, outToClient);
+		}
+	}
+	
+	public static void checkPlayer(String action, BufferedReader br, DataOutputStream dos){
+		String[] args = action.split(";");
+		if(args[0].equals("ADDPLAYER")){
+			Player player = new Player(args[1], br, dos, board);
+			players.add(player);
+			IngoingServer in = new IngoingServer(player, players);
+//			OutgoingServer out = new OutgoingServer(player);
+			String toClient ="";
+			for (Player p: players){
+				toClient += p.getName()+";"+p.getXpos()+";"+p.getYpos()+";"+p.getPoint()+";"
+			}
+			dos.writeBytes();
+		}
+		else if(args[0].equals("MOVEPLAYER")){
+			for(Player p : players){
+				if(p.getName().equals(args[0])){
+					
+				}
+			}
+		}
+		else if(args[0].equals("REMOVE")){
 			
 		}
 	}
-
 }
--- game/gameplayer.java
+++ game/gameplayer.java
@@ -7,7 +7,6 @@ public class gameplayer
 	private String wall = "w";
 	ArrayList<Player> players;
 
-	Player me;
 	ScoreList scoreList;
 	Screen screen;
 
@@ -36,61 +35,25 @@ public class gameplayer
 			{ "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w" },
 		};
 
-	public gameplayer(Player me, ScoreList scoreList, ArrayList<Player> players)
+	public gameplayer(ScoreList scoreList, ArrayList<Player> players)
 	{
-		this.me = me;
 		this.scoreList = scoreList;
 		this.players = players;
 
 		// Start new screen
-		screen = new Screen(level, me.getXpos(), me.getYpos());
+//		screen = new Screen(level, me.getXpos(), me.getYpos());
 		// Set screen to be visible
-		screen.setVisible(true);
+//		screen.setVisible(true);
 
 		// Add key listener - on keyClass
-		screen.addKeyListener(new KeyClass(this));
+//		screen.addKeyListener(new KeyClass(this));
 	}
 
-	public void PlayerMoved(String direction)
-	{
-		me.direction = direction;
-		int x = me.getXpos(), y = me.getYpos();
-
-		if (direction.equals("right")) {
-			x = me.getXpos() + 1;
-		}
-		if (direction.equals("left")) {
-			x = me.getXpos() - 1;
-		}
-		if (direction.equals("up")) {
-			y = me.getYpos() - 1;
-		}
-		if (direction.equals("down")) {
-			y = me.getYpos() + 1;
-		}
-
-		if (level[x][y].equals(wall))
-		{
-
-			// Take a point from player
-			me.subOnePoint();
-			// Move player on the board
-			scoreList.updateScoreOnScreenAll();
-
-		} else {
-
-			// Give player a point
-			me.addOnePoint();
-
-			// Update scorelist
-			scoreList.updateScoreOnScreenAll();
-			// Move player on the board
-			screen.movePlayerOnScreen(me.getXpos(), me.getYpos(), x, y, me.getDirection());
-
-			// Set players X Y on player object
-			me.setXpos(x);
-			me.setYpos(y);
-
-		}
+	public String[][] getLevel() {
+		return level;
 	}
+	
+	
+			
+//	
 }

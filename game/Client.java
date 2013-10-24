c8b17386f01dc9882bfd3320d0c0f7737733eefc
Mike Raunsbæks <raunsbaek@raunsbaek-2.local>
2013-10-24 14:02:45 +0200

    Username check

--- game/Client.java
+++ game/Client.java
@@ -15,69 +15,73 @@ public class Client
 		System.out.println("Input player name");
 		BufferedReader username = new BufferedReader(new InputStreamReader(System.in));
 
-
-		// Start connection to server
-		Socket clientSocket = new Socket("10.10.133.143", 7531);
-		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-
-		// Send add player to server
-		outToServer.writeBytes("ADDPLAYER;" + username.readLine() + '\n');
-		// Get response from server
-		String response = inFromServer.readLine();
-
-
-		// If server is not full
-		if(!response.equals("Server full"))
+		if(username != null)
 		{
-			int index = 0;
-			String[] settings = response.split(";");
-			ArrayList<Player> players = new ArrayList<Player>();
 
-			for(; index < settings.length; index += 5)
+			// Start connection to server
+			Socket clientSocket = new Socket("10.10.133.143", 7531);
+			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+
+			// Send add player to server
+			outToServer.writeBytes("ADDPLAYER;" + username.readLine() + '\n');
+			// Get response from server
+			String response = inFromServer.readLine();
+
+
+			// If server is not full
+			if(!response.equals("Server full"))
 			{
-				Player p = new Player(settings[index], Integer.parseInt(settings[index+3]));
-				p.setXpos(Integer.parseInt(settings[index+1]));
-				p.setYpos(Integer.parseInt(settings[index+2]));
-				p.setDirection(settings[index+4]);
-				players.add(p);
+				int index = 0;
+				String[] settings = response.split(";");
+				ArrayList<Player> players = new ArrayList<Player>();
+
+				for(; index < settings.length; index += 5)
+				{
+					Player p = new Player(settings[index], Integer.parseInt(settings[index+3]));
+					p.setXpos(Integer.parseInt(settings[index+1]));
+					p.setYpos(Integer.parseInt(settings[index+2]));
+					p.setDirection(settings[index+4]);
+					players.add(p);
+				}
+
+
+				// Create a scorelist
+				ScoreList scoreList = new ScoreList(players);
+				scoreList.setVisible(true);
+
+
+				// Gameplayet
+				Gameplayer game = new Gameplayer(scoreList, players);
+				Screen screen = new Screen(game.getLevel());
+				screen.setVisible(true);
+
+				// Key listener
+				KeyClass keyListener = new KeyClass(outToServer);
+				screen.addKeyListener(keyListener);
+
+				// Draw players
+				for(Player player: players)
+				{
+					screen.drawPlayer(player);
+				}
+
+
+				// IngoingClient needs gameplayer to make changes
+				IngoingClient inThread = new IngoingClient(inFromServer, game, screen);
+
+				// Shizzle on the thread
+				inThread.start();
+				inThread.join();
+			}
+			else {
+				System.out.println("Server is full");
 			}
 
+			// Close connection
+			clientSocket.close();
 
-			// Create a scorelist
-			ScoreList scoreList = new ScoreList(players);
-			scoreList.setVisible(true);
-
-
-			// Gameplayet
-			Gameplayer game = new Gameplayer(scoreList, players);
-			Screen screen = new Screen(game.getLevel());
-			screen.setVisible(true);
-
-			// Key listener
-			KeyClass keyListener = new KeyClass(outToServer);
-			screen.addKeyListener(keyListener);
-
-			// Draw players
-			for(Player player: players)
-			{
-				screen.drawPlayer(player);
-			}
-
-
-			// IngoingClient needs gameplayer to make changes
-			IngoingClient inThread = new IngoingClient(inFromServer, game, screen);
-
-			// Shizzle on the thread
-			inThread.start();
-			inThread.join();
 		}
-		else {
-			System.out.println("Server is full");
-		}
-
-		// Close connection
-		clientSocket.close();
 	}
 }

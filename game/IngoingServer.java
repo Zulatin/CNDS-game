3e368608d077c6574b5ac26d46f134af874ba8e1
Mike Raunsbæks <raunsbaek@raunsbaek-2.local>
2013-10-28 09:35:29 +0100

    Close connection works with no error

--- game/IngoingServer.java
+++ game/IngoingServer.java
@@ -16,85 +16,100 @@ public class IngoingServer extends Thread
 	{
 		while(true)
 		{
+			String getInFromClient = null;
 			try {
+				getInFromClient = player.getInFromClient().readLine();
+			} catch (IOException e1) {}
+
+
+			// Check if getInFromClient
+			if(getInFromClient == null)
+			{
+
+				// Close thread
+				Thread.currentThread().interrupt();
+				Server.players.remove(player);
+				return;
+
+			}else{
+
 				// Split string from client
-				String[] message = player.getInFromClient().readLine().split(";");
+				String[] message = getInFromClient.split(";");
 
 				// Find direction and command
 				direction = message[1];
 				command = message[0];
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 
 
-			if (command.equals("MOVEPLAYER"))
-			{
-				if (direction != null)
+				if (command.equals("MOVEPLAYER"))
 				{
-					player.setDirection(direction);
-					int x = player.getXpos(), y = player.getYpos();
-
-					if (direction.equals("right")) {
-						x = player.getXpos() + 1;
-					}
-					if (direction.equals("left")) {
-						x = player.getXpos() - 1;
-					}
-					if (direction.equals("up")) {
-						y = player.getYpos() - 1;
-					}
-					if (direction.equals("down")) {
-						y = player.getYpos() + 1;
-					}
-
-					String[][] level = player.getBoard().getLevel();
-					if(level[x][y].equals(wall))
+					if (direction != null)
 					{
+						player.setDirection(direction);
+						int x = player.getXpos(), y = player.getYpos();
 
-						// Take a point from player
-						player.subOnePoint();
-						// Update scoreboard
-						Server.scoreList.updateScoreOnScreenAll();
+						if (direction.equals("right")) {
+							x = player.getXpos() + 1;
+						}
+						if (direction.equals("left")) {
+							x = player.getXpos() - 1;
+						}
+						if (direction.equals("up")) {
+							y = player.getYpos() - 1;
+						}
+						if (direction.equals("down")) {
+							y = player.getYpos() + 1;
+						}
 
-					} else {
+						String[][] level = player.getBoard().getLevel();
+						if(level[x][y].equals(wall))
+						{
 
-						// Give a point to player
-						player.addOnePoint();
-						// Update scoreboard
-						Server.scoreList.updateScoreOnScreenAll();
+							// Take a point from player
+							player.subOnePoint();
+							// Update scoreboard
+							Server.scoreList.updateScoreOnScreenAll();
 
-						// Set player positions
-						player.setXpos(x);
-						player.setYpos(y);
+						} else {
+
+							// Give a point to player
+							player.addOnePoint();
+							// Update scoreboard
+							Server.scoreList.updateScoreOnScreenAll();
+
+							// Set player positions
+							player.setXpos(x);
+							player.setYpos(y);
+
+						}
+
+						// Create toClient string
+						String toClient = "";
+
+						for (Player getPlayer: Server.players)
+						{
+							toClient += getPlayer.getName() + ";" + getPlayer.getXpos() + ";" + getPlayer.getYpos() + ";" + getPlayer.getPoint() + ";" + getPlayer.getDirection() + ";";
+						}
+
+
+						try
+						{
+
+							// For-each players and write players to them
+							for (Player getPlayers: Server.players)
+								getPlayers.output(toClient);
+
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
 
 					}
+				}
 
-					// Create toClient string
-					String toClient = "";
-
-					for (Player getPlayer: Server.players)
-					{
-						toClient += getPlayer.getName() + ";" + getPlayer.getXpos() + ";" + getPlayer.getYpos() + ";" + getPlayer.getPoint() + ";" + getPlayer.getDirection() + ";";
-					}
-
-
-					try
-					{
-
-						// For-each players and write players to them
-						for (Player getPlayers: Server.players)
-							getPlayers.output(toClient);
-
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+				if (command.equals("REMOVEPLAYER"))
+				{
 
 				}
-			}
-
-			if (command.equals("REMOVEPLAYER"))
-			{
 
 			}
 		}
